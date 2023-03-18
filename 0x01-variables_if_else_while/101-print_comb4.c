49ff9fe452b2f1590745fc23bc7960023d0ebe1c
Andy Agbaraku <iamandyagbaraku>
2023-03-17 21:06:59 -0700

    fix errors

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,29 +8,29 @@
 
 int main(void)
 {
-	int ones;
-	int tens;
-	int hundreds;
+    int ones;
+    int tens;
+    int hundreds;
 
-	for(hundreds = '0'; hundreds <= '9'; hundreds++) /*increment hundreds*/
-	{
-		for (tens = (hundreds + 1 ); tens <= '9'; tens++) /*increment tens*/
-		{
-			for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
-			{
-				putchar(hundreds);
-				putchar(tens);
-				putchar(ones);
+    for(hundreds = '0'; hundreds <= '9'; hundreds++) /*increment hundreds*/
+    {
+        for (tens = (hundreds + 1 ); tens <= '9'; tens++) /*increment tens*/
+        {
+            for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
+            {
+                putchar(hundreds);
+                putchar(tens);
+                putchar(ones);
 
-				if (hundreds != '7' || tens != '8' || ones != '9') /*print commas*/
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
+                if (hundreds != '7' || tens != '8' || ones != '9') /*print commas*/
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
