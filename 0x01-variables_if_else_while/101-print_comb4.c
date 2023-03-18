71c1138e4858194341f71d1184eb87f96afdd1a9
Andy Agbaraku <iamandyagbaraku>
2023-03-17 20:26:54 -0700

    Advance projects

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ *main -  prints all possible different combinations of three digits.
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ones;
+	int tens;
+	int hundreds;
+
+	for(hundreds = '0'; hundreds <= '9'; hundreds++) /*increment hundreds*/
+	{
+		for (tens = (hundreds + 1 ); tens <= '9'; tens++) /*increment tens*/
+		{
+			for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
+			{
+				putchar(hundreds);
+				putchar(tens);
+				putchar(ones);
+
+				if (hundreds != '7' || tens != '8' || ones != '9') /*print commas*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
