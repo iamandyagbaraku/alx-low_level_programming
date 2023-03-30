6d400b9367f272882bf77930ac0cd335968c063c
Andy Agbaraku <iamandyagbaraku>
2023-03-29 22:26:04 -0700

    task 2 - 3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,29 +1,28 @@
 #include "main.h"
 
 /**
- *_strncat - copy a strings
+ *_strncpy - copy a string
  *@dest: input value
  *@src: input value
  *@n: input value
  *
- *Return: a pointer to the resulting string dest
+ *Return: dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int j;
 
-	i = 0;
-	while (i < n && src[i] != '\0')
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		dest[i] = src[i];
-		i++;
+		dest[j] = src[j];
+		j++;
 	}
 
-	while (i < n)
+	while (j < n)
 	{
-		dest[i] = '\0';
-		i++;
+		dest[j] = '\0';
+		j++;
 	}
 
 	return (dest);
