1351c191edc71cb61ae18785cb23e58aa12b513e
Andy Agbaraku <iamandyagbaraku>
2023-03-30 15:13:09 -0700

    Advance 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 /**
- * rot13 - encoder rot13
- * @s: pointer to string parameter
+ *rot13 - encoder rot13
+ *@s: pointer to string parameter
  *
- * Return: *s
+ *Return: *s
  */
- 
- char *rot13(char *s)
- {
+
+char *rot13(char *s)
+{
 	int i;
 	int j;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	
-	for (i=0; s[i] != '\0'; i++)
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j=0; j < 52; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
 			{
@@ -26,5 +26,6 @@
 			}
 		}
 	}
+
 	return (s);
- }
+}
