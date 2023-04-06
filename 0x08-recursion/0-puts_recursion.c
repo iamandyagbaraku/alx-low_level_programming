3be7fd2b785da43f23dc958e7a7e62846ce22e20
Andy Agbaraku <iamandyagbaraku>
2023-04-05 22:39:15 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,10 +5,13 @@
  * @s: input
  * Return: Always 0 (Success)
  */
-void _puts_recursion(char * s) {
-  if ( * s) {
+void _puts_recursion(char * s)
+{
+  if ( * s)
+  {
     _putchar( * s);
     _puts_recursion(s + 1);
-  } else
-    _putchar('\n');
+  }
+  else
+	_putchar('\n');
 }
